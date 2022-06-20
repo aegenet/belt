8cf231562827641e4bc61afadec01bf8812f7fb1
aegenet <20248096+aegenet@users.noreply.github.com>
2022-06-20 13:48:30 +0200

    test(input-date): fix tests tz

--- packages/belt-light-date/src/input-date.spec.ts
+++ packages/belt-light-date/src/input-date.spec.ts
@@ -1,6 +1,8 @@
 import * as assert from 'assert';
 import { dateToInputDate, dateToInputDateTime, inputDateTimeToDate, inputDateToDate } from './index';
 
+const isParisTZ = Intl.DateTimeFormat().resolvedOptions().timeZone === 'Europe/Paris';
+
 describe('dateToInputDate', () => {
   it('String', () => {
     // iso
@@ -14,9 +16,9 @@ describe('dateToInputDate', () => {
 describe('inputDateToDate', () => {
   it('String', () => {
     // - => Iso
-    assert.strictEqual(inputDateToDate('2015-12-25').toISOString(), '2015-12-24T23:00:00.000Z');
+    assert.strictEqual(inputDateToDate('2015-12-25').toISOString(), isParisTZ ? '2015-12-24T23:00:00.000Z' : '2015-12-25T00:00:00.000Z');
     // / => Local
-    assert.strictEqual(inputDateToDate('2015/12/25').toISOString(), '2015-12-24T23:00:00.000Z');
+    assert.strictEqual(inputDateToDate('2015/12/25').toISOString(), isParisTZ ? '2015-12-24T23:00:00.000Z' : '2015-12-25T00:00:00.000Z');
     assert.strictEqual(inputDateToDate('2018-06-12').toISOString(), '2018-06-11T22:00:00.000Z');
   });
 });
@@ -24,9 +26,9 @@ describe('inputDateToDate', () => {
 describe('dateToInputDateTime', () => {
   it('String', () => {
     // iso
-    assert.strictEqual(dateToInputDateTime('2015-12-25'), '2015-12-25T01:00');
+    assert.strictEqual(dateToInputDateTime('2015-12-25'), isParisTZ ? '2015-12-25T01:00' : '2015-12-25T00:00');
     // local
-    assert.strictEqual(dateToInputDateTime('2015/12/25'), '2015-12-25T00:00');
+    assert.strictEqual(dateToInputDateTime('2015/12/25'), isParisTZ ? '2015-12-25T00:00' : '2015-12-25T01:00');
     assert.strictEqual(dateToInputDateTime('2018-06-12T19:30'), '2018-06-12T19:30');
   });
 
@@ -34,7 +36,7 @@ describe('dateToInputDateTime', () => {
     const inputDateTime = dateToInputDateTime('2018-06-12T19:30');
     assert.strictEqual(inputDateTime, '2018-06-12T19:30');
     const revertToDate = inputDateTimeToDate(inputDateTime);
-    assert.strictEqual(revertToDate.toISOString(), '2018-06-12T17:30:00.000Z');
+    assert.strictEqual(revertToDate.toISOString(), isParisTZ ? '2018-06-12T17:30:00.000Z' : '2018-06-12T19:30:00.000Z');
   });
 });
 
@@ -42,9 +44,9 @@ describe('inputDateTimeToDate', () => {
   it('String', () => {
     assert.ok(inputDateTimeToDate(null));
     // - => Iso
-    assert.strictEqual(inputDateTimeToDate('2015-12-25T19:30').toISOString(), '2015-12-25T17:30:00.000Z');
+    assert.strictEqual(inputDateTimeToDate('2015-12-25T19:30').toISOString(), isParisTZ ? '2015-12-25T17:30:00.000Z' : '2015-12-25T19:30:00.000Z');
     // / => Local
-    assert.strictEqual(inputDateTimeToDate('2015/12/25T19:30').toISOString(), '2015-12-25T17:30:00.000Z');
-    assert.strictEqual(inputDateTimeToDate('2018-06-12T19:30').toISOString(), '2018-06-12T17:30:00.000Z');
+    assert.strictEqual(inputDateTimeToDate('2015/12/25T19:30').toISOString(), isParisTZ ? '2015-12-25T17:30:00.000Z' : '2015-12-25T19:30:00.000Z');
+    assert.strictEqual(inputDateTimeToDate('2018-06-12T19:30').toISOString(), isParisTZ ? '2018-06-12T17:30:00.000Z' : '2018-06-12T19:30:00.000Z');
   });
 });
