eb0cdf4e4487aa11738a23d24a0cd59335c234f0
aegenet <20248096+aegenet@users.noreply.github.com>
2022-09-04 10:31:56 +0200

    test(light-date): Node v16.17.0

--- packages/belt-light-date/src/light-date.spec.ts
+++ packages/belt-light-date/src/light-date.spec.ts
@@ -8,11 +8,11 @@ describe('light-date', () => {
   });
   it('todayStart', () => {
     assert.equal(lightDate.todayStart().getDate(), new Date().getDate());
-    assert.equal(lightDate.todayStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '20/04/2022, 00:00:00');
+    assert.equal(lightDate.todayStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '20/04/2022 00:00:00');
   });
   it('todayEnd', () => {
     assert.equal(lightDate.todayEnd().getDate(), new Date().getDate());
-    assert.equal(lightDate.todayEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '20/04/2022, 23:59:59');
+    assert.equal(lightDate.todayEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '20/04/2022 23:59:59');
   });
   it('tomorrow', () => {
     assert.equal(lightDate.tomorrow('2022-04-20T12:14:00.072Z').toISOString(), '2022-04-21T12:14:00.072Z');
@@ -21,59 +21,59 @@ describe('light-date', () => {
     assert.equal(lightDate.yesterday('2022-04-20T12:14:00.072Z').toISOString(), '2022-04-19T12:14:00.072Z');
   });
   it('tomorrowStart', () => {
-    assert.equal(lightDate.tomorrowStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '21/04/2022, 00:00:00');
+    assert.equal(lightDate.tomorrowStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '21/04/2022 00:00:00');
   });
   it('tomorrowEnd', () => {
-    assert.equal(lightDate.tomorrowEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '21/04/2022, 23:59:59');
+    assert.equal(lightDate.tomorrowEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '21/04/2022 23:59:59');
   });
   it('monthStart', () => {
-    assert.equal(lightDate.monthStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/04/2022, 00:00:00');
+    assert.equal(lightDate.monthStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/04/2022 00:00:00');
   });
   it('monthEnd', () => {
-    assert.equal(lightDate.monthEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '30/04/2022, 23:59:59');
+    assert.equal(lightDate.monthEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '30/04/2022 23:59:59');
   });
   it('nextMonthStart', () => {
-    assert.equal(lightDate.nextMonthStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/05/2022, 00:00:00');
+    assert.equal(lightDate.nextMonthStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/05/2022 00:00:00');
   });
   it('nextMonthEnd', () => {
-    assert.equal(lightDate.nextMonthEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '31/05/2022, 23:59:59');
+    assert.equal(lightDate.nextMonthEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '31/05/2022 23:59:59');
   });
   it('prevMonthStart', () => {
-    assert.equal(lightDate.prevMonthStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/03/2022, 00:00:00');
+    assert.equal(lightDate.prevMonthStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/03/2022 00:00:00');
   });
   it('prevMonthEnd', () => {
-    assert.equal(lightDate.prevMonthEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '31/03/2022, 23:59:59');
+    assert.equal(lightDate.prevMonthEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '31/03/2022 23:59:59');
   });
   it('yearStart', () => {
-    assert.equal(lightDate.yearStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/01/2022, 00:00:00');
+    assert.equal(lightDate.yearStart('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '01/01/2022 00:00:00');
   });
   it('yearEnd', () => {
-    assert.equal(lightDate.yearEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '31/12/2022, 23:59:59');
+    assert.equal(lightDate.yearEnd('2022-04-20T12:14:00.072Z').toLocaleString('fr'), '31/12/2022 23:59:59');
   });
   it('weekStart', () => {
-    assert.equal(lightDate.weekStart('2022-04-20', 'en-US').toLocaleString('fr'), '17/04/2022, 00:00:00');
-    assert.equal(lightDate.weekStart('2022-06-12', 'fr-FR').toLocaleString('fr'), '06/06/2022, 00:00:00');
-    assert.equal(lightDate.weekStart('2022-04-20', 'fr-FR').toLocaleString('fr'), '18/04/2022, 00:00:00');
+    assert.equal(lightDate.weekStart('2022-04-20', 'en-US').toLocaleString('fr'), '17/04/2022 00:00:00');
+    assert.equal(lightDate.weekStart('2022-06-12', 'fr-FR').toLocaleString('fr'), '06/06/2022 00:00:00');
+    assert.equal(lightDate.weekStart('2022-04-20', 'fr-FR').toLocaleString('fr'), '18/04/2022 00:00:00');
   });
   it('weekEnd', () => {
-    assert.equal(lightDate.weekEnd('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '24/04/2022, 23:59:59');
-    assert.equal(lightDate.weekEnd('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '23/04/2022, 23:59:59');
+    assert.equal(lightDate.weekEnd('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '24/04/2022 23:59:59');
+    assert.equal(lightDate.weekEnd('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '23/04/2022 23:59:59');
   });
   it('prevWeekStart', () => {
-    assert.equal(lightDate.prevWeekStart('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '11/04/2022, 00:00:00');
-    assert.equal(lightDate.prevWeekStart('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '10/04/2022, 00:00:00');
+    assert.equal(lightDate.prevWeekStart('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '11/04/2022 00:00:00');
+    assert.equal(lightDate.prevWeekStart('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '10/04/2022 00:00:00');
   });
   it('prevWeekEnd', () => {
-    assert.equal(lightDate.prevWeekEnd('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '17/04/2022, 23:59:59');
-    assert.equal(lightDate.prevWeekEnd('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '16/04/2022, 23:59:59');
+    assert.equal(lightDate.prevWeekEnd('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '17/04/2022 23:59:59');
+    assert.equal(lightDate.prevWeekEnd('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '16/04/2022 23:59:59');
   });
   it('nextWeekStart', () => {
-    assert.equal(lightDate.nextWeekStart('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '25/04/2022, 00:00:00');
-    assert.equal(lightDate.nextWeekStart('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '24/04/2022, 00:00:00');
+    assert.equal(lightDate.nextWeekStart('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '25/04/2022 00:00:00');
+    assert.equal(lightDate.nextWeekStart('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '24/04/2022 00:00:00');
   });
   it('nextWeekEnd', () => {
-    assert.equal(lightDate.nextWeekEnd('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '01/05/2022, 23:59:59');
-    assert.equal(lightDate.nextWeekEnd('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '30/04/2022, 23:59:59');
+    assert.equal(lightDate.nextWeekEnd('2022-04-20T12:14:00.072Z', 'fr-FR').toLocaleString('fr'), '01/05/2022 23:59:59');
+    assert.equal(lightDate.nextWeekEnd('2022-04-20T12:14:00.072Z', 'en-US').toLocaleString('fr'), '30/04/2022 23:59:59');
   });
 
   it('getIntlLocale', () => {
--- packages/belt-light-date/src/split-date.spec.ts
+++ packages/belt-light-date/src/split-date.spec.ts
@@ -8,8 +8,8 @@ describe('split-date', () => {
       dateFrom: '2022-04-20T12:14:00.072Z',
     });
 
-    assert.equal(dates.start.toLocaleString('fr'), '05/04/2022, 00:00:00');
-    assert.equal(dates.end.toLocaleString('fr'), '05/05/2022, 23:59:59');
+    assert.equal(dates.start.toLocaleString('fr'), '05/04/2022 00:00:00');
+    assert.equal(dates.end.toLocaleString('fr'), '05/05/2022 23:59:59');
   });
   it('splitDate week', () => {
     const locale = new Intl.NumberFormat().resolvedOptions().locale;
@@ -19,7 +19,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'week',
       }).start.toLocaleString('fr'),
-      locale === 'en-US' ? '03/04/2022, 00:00:00' : '04/04/2022, 00:00:00'
+      locale === 'en-US' ? '03/04/2022, 00:00:00' : '04/04/2022 00:00:00'
     );
     assert.equal(
       splitDate({
@@ -27,7 +27,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'week',
       }).end.toLocaleString('fr'),
-      locale === 'en-US' ? '30/04/2022, 23:59:59' : '01/05/2022, 23:59:59'
+      locale === 'en-US' ? '30/04/2022, 23:59:59' : '01/05/2022 23:59:59'
     );
   });
   it('splitDate month', () => {
@@ -37,7 +37,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'month',
       }).start.toLocaleString('fr'),
-      '01/04/2022, 00:00:00'
+      '01/04/2022 00:00:00'
     );
     assert.equal(
       splitDate({
@@ -45,7 +45,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'month',
       }).end.toLocaleString('fr'),
-      '31/05/2022, 23:59:59'
+      '31/05/2022 23:59:59'
     );
   });
   it('splitDate year', () => {
@@ -55,7 +55,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'year',
       }).start.toLocaleString('fr'),
-      '01/01/2022, 00:00:00'
+      '01/01/2022 00:00:00'
     );
     assert.equal(
       splitDate({
@@ -63,7 +63,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'year',
       }).end.toLocaleString('fr'),
-      '31/12/2022, 23:59:59'
+      '31/12/2022 23:59:59'
     );
   });
   it('splitDate 365d week', () => {
@@ -74,7 +74,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'week',
       }).start.toLocaleString('fr'),
-      locale === 'en-US' ? '17/10/2021, 00:00:00' : '18/10/2021, 00:00:00'
+      locale === 'en-US' ? '17/10/2021, 00:00:00' : '18/10/2021 00:00:00'
     );
     assert.equal(
       splitDate({
@@ -82,7 +82,7 @@ describe('split-date', () => {
         dateFrom: '2022-04-20T12:14:00.072Z',
         startEndOf: 'week',
       }).end.toLocaleString('fr'),
-      locale === 'en-US' ? '15/10/2022, 23:59:59' : '16/10/2022, 23:59:59'
+      locale === 'en-US' ? '15/10/2022, 23:59:59' : '16/10/2022 23:59:59'
     );
   });
   it('splitDate 365d', () => {
@@ -91,14 +91,14 @@ describe('split-date', () => {
         days: 365,
         dateFrom: '2022-04-20T12:14:00.072Z',
       }).start.toLocaleString('fr'),
-      '20/10/2021, 00:00:00'
+      '20/10/2021 00:00:00'
     );
     assert.equal(
       splitDate({
         days: 365,
         dateFrom: '2022-04-20T12:14:00.072Z',
       }).end.toLocaleString('fr'),
-      '19/10/2022, 23:59:59'
+      '19/10/2022 23:59:59'
     );
   });
 });
