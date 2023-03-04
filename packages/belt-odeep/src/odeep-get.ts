4e79f959ce8cff253386c4e67910016068d086c9
aegenet <20248096+aegenet@users.noreply.github.com>
2023-03-04 22:02:50 +0100

    fix(odeep-get): `safer` is more safer (property name)

--- packages/belt-odeep/src/odeep-get.ts
+++ packages/belt-odeep/src/odeep-get.ts
@@ -10,6 +10,8 @@ export type GetValueOptions = {
 
 /** ODeepGet */
 export class ODeepGet {
+  private readonly _safePropertyRE = /^[a-z0-9\-_]+$/i;
+
   private _jitCache: Map<string, (context: unknown) => unknown>;
 
   constructor() {
@@ -88,22 +90,37 @@ export class ODeepGet {
     let jit = `context`;
     let control = '';
     let step: string | number = '';
+    let inError: boolean = false;
+
     for (let i = 0; i < path.length; i++) {
       step = path[i];
       control += `${jit}.hasOwnProperty('${step}') && `;
       if (i === 0 && step === '#') {
         continue;
       }
+      if (!this._safePropertyRE.test(step as string)) {
+        if (!options.shallowError) {
+          throw new Error(`The property path (${path.join('.')}) seems invalid`);
+        } else {
+          inError = true;
+          break;
+        }
+      }
 
       jit += !isNaN(step as any) ? `[${step}]` : `.${step}`;
     }
 
-    jit = `if (${control}true) { return ${jit} } else { throw new Error('The property path (${path.join('.')}) seems invalid'); }`;
+    // Error but shallow
+    if (inError) {
+      return 'return undefined;';
+    } else {
+      jit = `if (${control}true) { return ${jit} } else { throw new Error('The property path (${path.join('.')}) seems invalid'); }`;
 
-    if (options.shallowError) {
-      jit = `try { ${jit}} catch { return undefined; }`;
+      if (options.shallowError) {
+        jit = `try { ${jit}} catch { return undefined; }`;
+      }
+      return jit;
     }
-    return jit;
   }
 
   /** If no memoize, we can get the value by a simple reduce */
@@ -126,10 +143,10 @@ export class ODeepGet {
     }
 
     if (options.shallowError) {
-      return pathFlow.reduce<any>((prev, curr) => (prev && (prev as object).hasOwnProperty(curr) ? prev[curr] : undefined), context);
+      return pathFlow.reduce<any>((prev, curr) => (prev && this._safePropertyRE.test(curr as string) && (prev as object).hasOwnProperty(curr) ? prev[curr] : undefined), context);
     } else {
       return pathFlow.reduce<any>((prev, curr) => {
-        if (prev && (prev as object).hasOwnProperty(curr)) {
+        if (prev && this._safePropertyRE.test(curr as string) && (prev as object).hasOwnProperty(curr)) {
           return prev[curr];
         } else {
           throw new Error(`The property path (${pathFlow.join('.')}) seems invalid`);
