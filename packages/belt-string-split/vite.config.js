4bd9b524e149b8009df90757fd5a0d8c04874336
aegenet <20248096+aegenet@users.noreply.github.com>
2023-08-14 00:05:56 +0200

    chore(vite.config.js): fix build belt-string-split

--- packages/belt-string-split/vite.config.js
+++ packages/belt-string-split/vite.config.js
@@ -4,6 +4,6 @@ const { config } = require('./../../.build/vite.configurator');
 export default config({
   cwd: __dirname,
   libName: '@aegenet/belt-string-split',
-  entryPoint: 'belt-string-split/src/index',
+  entryPoint: 'index',
   nodeExternal: true,
 });
