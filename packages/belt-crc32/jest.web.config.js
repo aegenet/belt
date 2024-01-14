b423f4cc8b33dec94f1a1fc62c7d9f586776a68e
aegenet <20248096+aegenet@users.noreply.github.com>
2024-01-14 17:49:05 +0100

    chore(jest.config.js): rem comments

--- packages/belt-crc32/jest.web.config.js
+++ packages/belt-crc32/jest.web.config.js
@@ -2,7 +2,6 @@
 const configurator = require('../../.build/jest.web.configurator');
 
 module.exports = configurator({
-  /** Si nous sommes entrain de builder tous les projets nous voulons mettre les coverages aux mêmes endroits, sinon, c'est par projet */
   directory: __dirname,
   testRegex: 'src/(.*)\\.spec\\.(ts|js)$',
 });
--- packages/belt-string-split/jest.config.js
+++ packages/belt-string-split/jest.config.js
@@ -2,6 +2,5 @@
 const configurator = require('../../.build/jest.configurator');
 
 module.exports = configurator({
-  /** Si nous sommes entrain de builder tous les projets nous voulons mettre les coverages aux mêmes endroits, sinon, c'est par projet */
   directory: __dirname,
 });
