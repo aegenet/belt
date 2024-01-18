fbe255c3d9b65b350d5244e785f227b0fc0f631e
aegenet <20248096+aegenet@users.noreply.github.com>
2024-01-18 09:25:21 +0100

    fix(b-fetch): clear the dns cache if request failed

--- packages/belt-fetch/src/node/client/b-fetch.ts
+++ packages/belt-fetch/src/node/client/b-fetch.ts
@@ -4,20 +4,47 @@ import type { bFetchOptions } from '../../common/client/b-fetch-options';
 import { bFetchApplyDefaultOptions } from '../../common/client/b-fetch-default-options';
 import { LOCALHOST } from '../../common/client/b-fetch-utils';
 import { bFetchGetIPsFromDNS } from './b-fetch-get-ips-from-dns';
+import { bFetchSharedCache } from './b-fetch-shared-cache';
 
 /** Fetch with timeout and an async dns resolver */
 export async function bFetch(input: RequestInfo | URL, init: RequestInit = {}, options: bFetchOptions = {}): Promise<Response> {
   bFetchApplyDefaultOptions(options);
 
-  let origHost: string;
+  let origHostname: string;
   let urlInput: URL;
   if (typeof input === 'string' || input instanceof URL) {
     urlInput = new URL(input);
-    origHost = urlInput.host;
+    const origHost = urlInput.host;
+    origHostname = urlInput.hostname;
 
     // Check if it is a domain or not an ip (v4/v6)
-    if (options.replaceDNSByIP && urlInput.hostname !== LOCALHOST && isIP(urlInput.hostname) === 0) {
-      const ips: string[] = await bFetchGetIPsFromDNS(urlInput, options);
+    if (options.replaceDNSByIP && origHostname !== LOCALHOST && isIP(origHostname) === 0) {
+      let ips: string[] | undefined;
+
+      // DNS from cache?
+      if (options.dnsCacheTTL && bFetchSharedCache.has(origHostname)) {
+        const cache = bFetchSharedCache.get(origHostname);
+        if (cache!.eAt > Date.now()) {
+          ips = cache!.ips;
+        } else {
+          bFetchSharedCache.delete(origHostname);
+        }
+      }
+
+      if (!ips) {
+        if ((ips = await bFetchGetIPsFromDNS(urlInput, options))) {
+          if (options.dnsCacheTTL) {
+            // Cache DNS
+            bFetchSharedCache.set(origHostname, {
+              ips,
+              eAt: Date.now() + options.dnsCacheTTL,
+            });
+          }
+        } else {
+          throw new Error(`bFetch: ENOTFOUND ${origHostname}`);
+        }
+      }
+
       urlInput.hostname = ips[0];
 
       init.headers ??= {};
@@ -32,5 +59,11 @@ export async function bFetch(input: RequestInfo | URL, init: RequestInit = {}, o
   return await fetch(urlInput, {
     ...init,
     signal: AbortSignal.timeout(options.timeout!),
+  }).catch(error => {
+    if (options.dnsCacheTTL && bFetchSharedCache.has(origHostname)) {
+      // Clear DNS cache if request failed
+      bFetchSharedCache.delete(origHostname);
+    }
+    throw error;
   });
 }
